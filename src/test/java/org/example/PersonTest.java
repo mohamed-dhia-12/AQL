07eb42e28fc109932c54128e917c777406555efa
dhia <dhia@gmail.com>
2025-04-09 15:25:11 +0100

    TP 0 EXO 1

--- src/test/java/org/example/PersonTest.java
+++ src/test/java/org/example/PersonTest.java
@@ -1,7 +1,20 @@
 package org.example;
-
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PersonTest {
-
+    @Test
+    void  getFullName_ReturnCorrectFormat(){
+        Person P = new Person("Mohamed Dhia","Helaimia",12);
+        assertEquals("Mohamed Dhia Helaimia",P.getFullName());
+    }
+    @Test
+    void isAdult_returnTrue18ormore(){
+        assertTrue((new Person("Dhia","Adult",18)).isAdult());
+        assertTrue((new Person("Mohamed","Adult",26)).isAdult());
+    }
+    @Test
+    void isAdult_returnFalse17orless(){
+        assertFalse((new Person("Mounir","!Adult",17)).isAdult());
+    }
 }
